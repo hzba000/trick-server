74086c8a410866e7dd16aa2b830382e8e0b5bf48
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2019-03-08 20:58:47 -0800

    Modified files sync

--- config.js
+++ config.js
@@ -2,3 +2,5 @@ exports.CLIENT_ORIGIN = process.env.PORT || 8080;
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://testuser1:testuser1@ds255794.mlab.com:55794/trick-server';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://testuser1:testuser1@ds255794.mlab.com:55794/trick-server-test';
 exports.PORT = process.env.PORT || 3000;
+
+//You really need a local database
--- server.js
+++ server.js
@@ -1,25 +1,95 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 const app = express();
+const { Title } = require('./title.model.js');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 const cors = require('cors');
-const {CLIENT_ORIGIN, PORT} = require('./config');
-
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
+const {CLIENT_ORIGIN, PORT, DATABASE_URL, TEST_DATABASE_URL} = require('./config');
+//log HTTP layer
+app.use(morgan('common'));
+//Body Parsing Middleware
+app.use(express.json());
+//Public Assets
+app.use(express.static('public'));
+app.use(cors({origin: CLIENT_ORIGIN}));
 
 app.get('/money', (req,res) => {
-    res.json({Hozefa: message})
+    res.json({"DRINK": "DRINK"})
 })
 
 app.get('/api/*', (req, res) => {
   res.json({ok: true});
 });
 
+mongoose.Promise = global.Promise;
 
+//SUBMIT A TITLE
+app.post('/addtitle', jsonParser ,(request, response) => {
+    const newTitle = {
+        title: request.body.title
+    };
+    Title.create(newTitle)
+        .then(newTitle => {return response.json(newTitle)})
+});
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.get('/jumpy',(request, response) => {
+    Title.find()
+        .then(titles => {
+            return response.json(titles.map(title => title.serialize()))
+        })
+})
 
-module.exports = {app};
+let server;
+
+// SERVER SETUP
+function startServer(testEnv) {
+    return new Promise((resolve, reject) => {
+        let databaseUrl;
+  
+        if (testEnv) { //testEnv is a boolean passed true when we are using the testing database
+            databaseUrl = TEST_DATABASE_URL;
+        } else {
+            databaseUrl = DATABASE_URL;
+        }
+        mongoose.connect(databaseUrl, { useNewUrlParser: true }, err => {
+            if (err) {
+                console.error(err);
+                return reject(err);
+            } else {
+                server = app.listen(PORT, () => {
+                    console.log(`Express server listening on http://localhost:${PORT}`);
+                    resolve();
+                }).on('error', err => {
+                    mongoose.disconnect();
+                    console.error(err);
+                    reject(err);
+                });
+            }
+        });
+    });
+  }
+  
+  function stopServer() {
+    return mongoose
+        .disconnect()
+        .then(() => new Promise((resolve, reject) => {
+            server.close(err => {
+                if (err) {
+                    console.error(err);
+                    return reject(err);
+                } else {
+                    console.log('Express server stopped.');
+                    resolve();
+                }
+            });
+        }));
+  }
+  
+  if (require.main === module) {
+    startServer().catch(err => console.error(err));
+  }
+  
+  module.exports = { startServer, app, stopServer };
